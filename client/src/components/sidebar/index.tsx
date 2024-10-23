85a050d5cd6e09ec0f47783d2901442c236ce523
Louis Bellefemine <bellefemine.louis@gmail.com>
2024-10-23 17:20:40 +0200

    client : basic layout desktop done | #1

---
+++ client/src/components/sidebar/index.tsx
@@ -0,0 +1,7 @@
+export function Sidebar() {
+  return (
+    <div className={"flex"}>
+      <div className={"p-4"}>SideBar</div>
+    </div>
+  )
+}
---
+++ client/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "#1C1D1E",
+                dark: "#22252A",
+                light: "#2F343E",
+                primary: "#7A81A6",
+                white: "#FFFFFF",
+                red: "#A67A7A",
+                green: "#7CA67A",
+            }
+        },
+    },
+    plugins: [],
+}
