52dba1b7099f79e17ebf0a2e8c74ee9bb2fe889f
Louis Bellefemine <bellefemine.louis@gmail.com>
2024-11-10 16:08:03 +0100

    💄 Basic desktop layout

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { Header, Sidebar } from '@/app/components';
 
 export const metadata: Metadata = {
   title: 'Kusodu',
@@ -13,7 +14,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={'flex flex-col bg-dark h-dvh w-dvw text-white'}>
+        <header className={'m-4 mb-2'}>
+          <Header />
+        </header>
+        <div className={'flex grow '}>
+          <aside className={'m-4 mt-2'}>
+            <Sidebar />
+          </aside>
+          <main className={'bg-background grow rounded-xl m-4 mt-2 p-4'}>
+            {children}
+          </main>
+        </div>
+      </body>
     </html>
   );
 }
