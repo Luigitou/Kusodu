c4549ca6af9fd5e74ab6ecb0cfa7bb8f5077832c
Louis Bellefemine <bellefemine.louis@gmail.com>
2024-11-12 16:46:19 +0100

    💄 Mobile menu & responsive header

--- client/app/ui/Header/index.tsx
+++ client/app/ui/Header/index.tsx
@@ -1,12 +1,31 @@
 'use client';
 
+import { HeaderButtons } from '@/components';
+import { Menu01Icon, UserIcon } from '@/icons';
+import { Sidebar } from '@/ui';
+import { useState } from 'react';
+
 export function Header() {
+  const [openSidebar, setOpenSidebar] = useState(false);
+
   return (
     <div className={'flex items-center justify-between'}>
       <h1 className={'text-3xl md:px-8'}>Kusodu.</h1>
-      <div>
-        <button>Profile</button>
+      <div className={'hidden md:block'}>
+        <HeaderButtons label={'Profile'} icon={<UserIcon />} href={'/login'} />
       </div>
+      <div className={'block md:hidden'}>
+        <HeaderButtons
+          label={'Profile'}
+          icon={<Menu01Icon />}
+          click={() => setOpenSidebar(true)}
+        />
+      </div>
+      {openSidebar && (
+        <div className={'block md:hidden'}>
+          <Sidebar isMobile={true} close={() => setOpenSidebar(false)} />
+        </div>
+      )}
     </div>
   );
 }
