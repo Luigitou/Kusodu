06eae5472d194366a76bd785645b441169505d99
Louis Bellefemine <bellefemine.louis@gmail.com>
2024-11-24 17:40:13 +0100

    🚧 Get grid endpoint done

--- api/src/grid/grid.controller.ts
+++ api/src/grid/grid.controller.ts
@@ -1,26 +1,28 @@
-import {
-  Controller,
-  Get,
-  InternalServerErrorException,
-  Param,
-} from '@nestjs/common';
-import {
-  Difficulty,
-  GeneratedGrid,
-  GridGeneratorService,
-} from '@app/grid-generator';
+import { Controller, Get, Param } from '@nestjs/common';
+import { Difficulty } from '@app/grid-generator';
 import { GetGridDto } from './dto/getGridDto';
 import { GridService } from './grid.service';
 
 @Controller('grid')
 export class GridController {
-  constructor(
-    private readonly gridGeneratorService: GridGeneratorService,
-    private readonly gridService: GridService,
-  ) {}
+  constructor(private readonly gridService: GridService) {}
 
   @Get(':difficulty')
   async getGrid(@Param() params: GetGridDto) {
+    const getGridFromDb = await this.gridService.findGridByDifficulty(
+      Difficulty[params.difficulty],
+    );
+
+    if (getGridFromDb) {
+      return getGridFromDb;
+    } else {
+      return this.gridService.generateGrid(Difficulty[params.difficulty]);
+    }
+  }
+
+  /*
+  @Get(':difficulty/logged')
+  async loggedUserGetGrid(@Param() params: GetGridDto) {
     const getGridFromDb = await this.gridService.findUnusedGrid(
       Difficulty[params.difficulty],
       user,
@@ -29,26 +31,8 @@ export class GridController {
     if (getGridFromDb) {
       return getGridFromDb;
     } else {
-      let newGrid: GeneratedGrid;
-      let counter = 0;
-      let success = false;
-
-      while (!success) {
-        try {
-          newGrid = await this.gridGeneratorService.generateNewGrid(
-            Difficulty[params.difficulty],
-          );
-          success = true;
-        } catch (_error) {
-          counter++;
-          if (counter > 5) {
-            return new InternalServerErrorException(
-              `Impossible to generate grid: ${counter} attempts`,
-            );
-          }
-        }
-      }
-      return newGrid;
+      return this.gridService.generateGrid(Difficulty[params.difficulty]);
     }
   }
+  */
 }
--- api/src/grid/grid.service.ts
+++ api/src/grid/grid.service.ts
@@ -1,10 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { PrismaService } from '@app/prisma';
 import { User } from '@prisma/client';
+import {
+  Difficulty,
+  GeneratedGrid,
+  GridGeneratorService,
+} from '@app/grid-generator';
 
 @Injectable()
 export class GridService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly gridGeneratorService: GridGeneratorService,
+    private readonly prismaService: PrismaService,
+  ) {}
+
+  async generateGrid(difficulty: Difficulty) {
+    let newGrid: GeneratedGrid;
+    let counter = 0;
+    let success = false;
+
+    while (!success) {
+      try {
+        newGrid = await this.gridGeneratorService.generateNewGrid(difficulty);
+        success = true;
+      } catch (_error) {
+        counter++;
+        if (counter > 5) {
+          return new InternalServerErrorException(
+            `Impossible to generate grid: ${counter} attempts`,
+          );
+        }
+      }
+    }
+    return newGrid;
+  }
 
   async findUnusedGrid(difficulty: number, user: User) {
     const grids = await this.prismaService.grid.findMany({
@@ -15,6 +44,21 @@ export class GridService {
             userId: user.id,
           },
         },
+      },
+    });
+
+    if (grids.length === 0) {
+      return null;
+    } else {
+      const randomIndex = Math.floor(Math.random() * grids.length);
+      return grids[randomIndex];
+    }
+  }
+
+  async findGridByDifficulty(difficulty: number) {
+    const grids = await this.prismaService.grid.findMany({
+      where: {
+        difficulty,
       },
     });
 
