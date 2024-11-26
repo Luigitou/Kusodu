8b74c1d83274a66364ae098bf82e7492172fa6c0
Louis Bellefemine <bellefemine.louis@gmail.com>
2024-11-26 20:28:47 +0100

    ✏️ Set tokens to be valid for 1h

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '2s' },
+      signOptions: { expiresIn: '1h' },
     }),
   ],
   controllers: [AuthController],
