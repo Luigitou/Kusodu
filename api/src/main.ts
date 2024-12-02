5d5048cf1e128f3ab397179fe2d8c1541b312914
Louis Bellefemine <bellefemine.louis@gmail.com>
2024-12-02 19:55:47 +0100

    🔧 Fix cors errors

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,6 +5,20 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
--- client/app/_services/api.ts
+++ client/app/_services/api.ts
@@ -4,10 +4,11 @@ import { toast } from 'react-toastify';
 import { refreshService } from '@/_services/auth';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
+  withCredentials: true,
 });
 
 // Automatically add the token to the request headers
