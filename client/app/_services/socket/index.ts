9edef251ee1b2eb68d24ae4fbb3c08f2cf8a9f37
Louis Bellefemine <bellefemine.louis@gmail.com>
2024-12-19 18:09:27 +0100

    ✏️ Fix eslint error on build

--- client/app/_services/socket/index.ts
+++ client/app/_services/socket/index.ts
@@ -1,12 +1,9 @@
 import { io, Socket } from 'socket.io-client';
-import { useStore } from '@/_store';
 
 let socket: Socket | null = null;
 
 export const getSocket = (): Socket | null => {
   if (typeof window === 'undefined') return null;
-
-  const token = useStore.getState().token;
 
   if (!socket) {
     socket = io(process.env.NEXT_PUBLIC_API_URL, {
